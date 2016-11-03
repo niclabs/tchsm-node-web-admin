86629d4c6b72915570b9147f0670d84988d0bf8d
raticate <caterina.munoz.v@gmail.com>
2016-11-03 14:28:43 -0300

    added node public key to config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class DefaultConfig(object):
     DEBUG = True
     CSRF_ENABLED = True
-    SECRET_KEY = '[\xd7\xda\x0cZ\x8a8\x8b\xb1g~\x0b\x86\x96\xf5\xe3\xa5Q\xfb\x86\x03\xb2\xfc'
+    SECRET_KEY = 'app secret key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////home/caterina/node_1.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    NODE_PUBLIC_KEY = 'node public key'
