f9e657b8c909f418f7801bfb2203447bdb5b298e
Francisco Montoto <fmontotomonroy@gmail.com>
2016-11-28 17:26:36 -0300

    add if name == main to run uwsgi

--- runserver.py
+++ runserver.py
@@ -1,4 +1,7 @@
-from nodeadmin import app, db 
+import nodeadmin
 
-db.create_all()
-app.run(debug=False)
+nodeadmin.db.create_all()
+app = nodeadmin.app
+
+if __name__ == "__main__":
+    app.run(debug=False)
