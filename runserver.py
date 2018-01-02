eb7b2a37b6d0cf423aba3116c0507bd7d8b60368
Felipe Quintanilla <fquintan.m@gmail.com>
2018-01-02 14:46:01 -0300

    Admin creation now happens on any execution of runserver.
    Previously the admin creation code happened only when runserver was run as main,
    this prevented it from executing it when running with uwsgi.

--- runserver.py
+++ runserver.py
@@ -4,15 +4,16 @@ from sqlalchemy.exc import SQLAlchemyError
 nodeadmin.db.create_all()
 app = nodeadmin.app
 
+admin_user = nodeadmin.User(app.config['ADMIN_EMAIL'], app.config['ADMIN_PASSWORD'])
+results = nodeadmin.db.session.query(nodeadmin.User).filter_by(email=admin_user.email).all()
+if len(results) == 0:
+    print("Couldn't find admin user. Creating one with credentials from config")
+    try:
+        nodeadmin.db.session.add(admin_user)
+        nodeadmin.db.session.commit()
+    except SQLAlchemyError as e:
+        print("Failed to create admin user")
+        print(e)
+
 if __name__ == "__main__":
-    admin_user = nodeadmin.User(app.config['ADMIN_EMAIL'], app.config['ADMIN_PASSWORD'])
-    results = nodeadmin.db.session.query(nodeadmin.User).filter_by(email=admin_user.email).all()
-    if len(results) == 0:
-        print("Couldn't find admin user. Creating one with credentials from config")
-        try:
-            nodeadmin.db.session.add(admin_user)
-            nodeadmin.db.session.commit()
-        except SQLAlchemyError as e:
-            print("Failed to create admin user")
-            print(e)
     app.run(debug=False)
